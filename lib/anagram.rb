e5d1cd264f36a0f13f1beede8bbcaf55ca8f6aef
Mollie Dickenson <molliedickenson@gmail.com>
2021-05-18 19:40:54 -0400

    commit tree 8ef290b9c540f64f82d82af90f7199e02f483d4d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    attr_reader :string
+
+    def initialize(string)
+        @string = string
+    end
+
+    def match(array)
+        array.select{|w| w.split('').sort == @string.split('').sort}
+    end
+
+end
